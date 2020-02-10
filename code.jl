c913a626415c1f89a1619c0877e5b8708cb8072c
VasilisAle <42702373+VasilisAle@users.noreply.github.com>
2020-02-10 17:30:20 +0100

    Update code.jl
    New update: few changes in the classify function

--- code.jl
+++ code.jl
@@ -6,66 +6,47 @@ using ProgressMeter
 using StaticArrays
 using BenchmarkTools
 using PointCloudRasterizers
+using NearestNeighbors #use it for spatial search
 
-#working datasets
-workdir = dirname(@__FILE__)
-const filenn_NL3_clipped = joinpath(workdir, "NL3_clipped_water_cropheight_pointformat3_classification_sorted.laz")
-const filenn_NL1_3_splitted = joinpath(workdir, "3_splitted_sorted_pointformat3.laz")
-const filenn_NL1_3_splitted_las = File{format"LAZ_"}(filenn_NL1_3_splitted)
 
-#read specific LAS file
-header, points = LazIO.load(filenn_NL1_3_splitted_las) #point cloud
-n = length(points) #length
-
-#output files
-outputname = "_colored_points"
-writefiles = joinpath(workdir, "3_splitted_sorted_pointformat3" * outputname * ".laz")
-
-#read dataset
-ds = LazIO.open(filenn_NL1_3_splitted) #dataset
-df = DataFrame(ds) #dataframe, I can not retrieve right info ex. x coords without offset
-
-#LazIO points
-# pp = collect(ds)
-
-function getpulse(points)
+#get the pulse
+function classify(points)
     prev_point = points[1]
-    first_point = points[1]
-    pulse =  Dict{Int,String}()
+    pulse =  Dict{Int,String}() #dictionary with indexes and tags
     n = 0
     for (i,p) in enumerate(points)
+        point = p
         inten = intensity(p)
         rn = return_number(p)
         nr = number_of_returns(p)
-        dt = gps_time(first_point) - gps_time(prev_point)
-
-        # global n = n + 1
-        # println(rn," ",nr)
-
-        if (nr > 1) &&  (rn == n + 1)
+        dt = gps_time(point) - gps_time(prev_point)
+        #rn = return number, nr = number_of_returns
+        if (rn == n + 1) && (nr > 1)
             if rn == 1
                 pulse[i] = "firstpoint"
-            elseif dt < 1e-7
+            elseif rn < nr && dt < 1e-7
                 pulse[i] = "nextpoint"
-            elseif rn == nr
+            elseif rn == nr && dt < 1e-7
                 pulse[i] = "lastpoint"
-                n = 0
-            else
-                n+=1
             end
-            # println(" I added a point ", i)
-        else
+            if nr == rn
+                n = 0
+            end
+        elseif nr == 1 && rn == 1
+            pulse[i] = "firstpoint"
             n = 0
+        else
             pulse[i] ="unclassified"
-            # println(" Point not added ", i)
+            n = 0
         end
+        n+=1
         prev_point = p
     end
     pulse
 end
 
 #write point cloud
-function write_output(writefiles,ds,pulse)
+function write_output(writefiles,ds, pulse)
     LazIO.write(writefiles, ds.header) do io
         for (i,p) in enumerate(ds) #I need to interate only to dataset, since it is Lazpoints
             allkeys = keys(pulse)
@@ -85,5 +66,33 @@ function write_output(writefiles,ds,pulse)
     end
 end
 
-pulse = getpulse(points)
-write_output(writefiles,ds,pulse)
+##MAIN##
+#working with 4 different datasets
+workdir = dirname(@__FILE__)
+# dataname_1 = "NL3_clipped_water_cropheight_pointformat3_classification_sorted"
+# dataname_2 = "3_splitted_sorted_pointformat3"
+# dataname_3 = "12_splitted_sorted_pointformat3_clipZ"
+dataname_4 = "14_splitted_sorted_pointformat3_clipZ"
+
+# const filenn_NL3 = joinpath(workdir,dataname_1 * ".laz")
+# const filenn_NL3_las = File{format"LAZ_"}(filenn_NL3)
+
+const filenn_NL1 = joinpath(workdir,dataname_4 * ".laz")
+const filenn_NL1_las = File{format"LAZ_"}(filenn_NL1)
+
+#read specific LAS file
+header, points = LazIO.load(filenn_NL1_las) #point cloud as LAS format
+n = length(points) #length
+
+#output files
+outputname = "_classified"
+writefiles = joinpath(workdir, dataname_4 * outputname * ".laz")
+
+#read dataset
+ds = LazIO.open(filenn_NL1)#dataset
+
+#pulse
+classified = classify(points)
+
+#write output
+write_output(writefiles,ds,classified)
